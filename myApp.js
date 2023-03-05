e208aa56514457ab3980ff1da013df729ad2cfdd
Jeavo <108761427+Jeavo@users.noreply.github.com>
2023-03-05 19:51:55 +0000

    Practice 1

--- myApp.js
+++ myApp.js
@@ -1,5 +1,6 @@
 let express = require('express');
 let app = express();
+console.log('Hello World');
 
 
 
@@ -36,4 +37,5 @@ let app = express();
 
 
 
- module.exports = app;
+
+module.exports = app;
