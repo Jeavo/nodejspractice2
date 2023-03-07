54d7f6003d89fbb9cb5d0f89e20a8e9bd8977260
Jeavo <108761427+Jeavo@users.noreply.github.com>
2023-03-07 22:10:30 +0000

    new update

--- myApp.js
+++ myApp.js
@@ -2,6 +2,14 @@ let express = require('express');
 let app = express();
 console.log('Hello World');
 
+app.get("/", (req, res) => {
+  let absolutePath = __dirname + '/views/index.html'
+  res.sendFile(absolutePath);
+})
+
+let newPath = __dirname + '/public'
+app.use("/public", express.static(newPath))
+
 
 
 
